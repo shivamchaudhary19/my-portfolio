6eee4ed669ee1f9b00c876b865d5caeb215da226
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-08-07 07:33:55 +0530

    feat: create initial portfolio page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import ThemeBtn from './components/ThemeBtn'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import About from './components/About'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +14,12 @@ function App() {
   return (
     <>
     <h1>Hello Brother</h1>
-    <ThemeBtn />
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    <Contact />
+    <Footer />
     </>
   )
 }
